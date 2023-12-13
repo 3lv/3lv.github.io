e1cba7a27a62496568f95a51c721cb4d79bc53b0
3lv <cristian.vlad.vladutu@gmail.com>
2023-12-14 01:59:11 +0200

    Online menu

---
+++ test.sh
@@ -0,0 +1,29 @@
+option_list=("Bootstrap entire system" \
+"neovim installation with plugins" \
+"reverse shell for windows(not working)" \
+)
+
+cyan="\033[36m"
+green="\033[32m"
+reset="\033[0m"
+
+echo -e "${cyan}3lv online menu${reset}"
+
+install() {
+	curl https://3lv.github.io/$1 | sudo sh
+}
+
+select option in "${option_list[@]}"; do
+	case option in
+		1) echo "Selected bootstrap"
+			install("bs")
+			;;
+		2) echo "Selected neovim"
+			install("nvim")
+			;;
+		3) echo "Selected reverse shell for windows"
+			install("rw")
+			;;
+		*) echo "Unknown option"
+	esac
+done
